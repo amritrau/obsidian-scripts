3b02fad26d97eeb36b543e8410809663b054e524
Amrit Rau <amritrau@gmail.com>
2021-04-25 12:27:57 -0700

    Add rename.py

---
+++ rename.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+"""
+rename.py
+@amritrau
+
+Usage: rename.py VAULT
+Options:
+  --help            Show this message and exit.
+
+Rename all Markdown files that don't follow the `YYYYMMDD-note-title.md`
+naming convention. Do not modify filenames that already match the pattern
+(even if their title is out of date).
+"""
+
+import click
+import frontmatter
+import logging
+import os
+import pathlib
+
+from constants import INDEX_PREFIX
+from utils import slugify, get_zk_prefix, hash_note
+
+logging.basicConfig(level=logging.INFO)
+
+
+@click.command()
+@click.argument('vault')
+def main(vault):
+    filenames = list(pathlib.Path(vault).rglob("*.md"))
+    n_files, n_renamed = len(filenames), 0
+    pattern = r"^\d{8}-(.*)\.md$"  # YYYYMMDD-note-title.md
+
+    for filename in filenames:
+        basename = os.path.basename(filename)
+        if not re.match(pattern, basename):
+            prefix = get_zk_prefix(filename)
+            
+            # Extract the title from the first line of the file
+            note = frontmatter.load(filename)
+            heading = note.content.split('\n')[0]
+            match = re.match(r"^# (.*)$", heading)
+            if match:
+                slug = slugify(match.group(1))
+            else:
+                # Generate a unique title based on the note's contents
+                slug = f"untitled-{hash_note(filename)}"
+            
+            new_name = f"{prefix}-{slug}.md"
+            
+            # Rename file
+            new_filename = str(filename).replace(basename, new_name)
+            os.rename(filename, new_filename)
+            n_renamed +=1
+
+    logging.info(f"Renamed {n_renamed} of {n_files} files.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""
+utils.py
+@amritrau
+
+Holds global utilities.
+"""
+
+import datetime
+import hashlib
+import frontmatter
+import pathlib
+import re
+import unicodedata
+
+
+def slugify(value):
+    """From StackOverflow #5574042"""
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub('[^\w\s-]', '', value).strip().lower()
+    return re.sub('[-\s]+', '-', value)
+
+
+def get_zk_prefix(filename: pathlib.Path, mode='ctime'):
+    """Get an 8-digit Zettelkasten-style prefix from a filepath"""
+    if mode == 'ctime':
+        dt = filename.stat().st_ctime
+    elif mode == 'mtime':
+        dt = filename.stat().st_ctime
+    else:
+        raise ValueError(f"Mode not recognized: {mode}")
+
+    return datetime.datetime.fromtimestamp(dt).strftime("%Y%m%d")
+
+
+def hash_note(filename: pathlib.Path, n=16):
+    """Get a unique n-hexdigit hash of the note's contents"""
+    note = frontmatter.load(filename)
+    sha = hashlib.sha256()
+    sha.update(str(note.content).encode())
+    sha.update(str(get_zk_prefix(filename)).encode())
+    return str(sha.hexdigest())[:n]
